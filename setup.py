9937dd0aa7bbad731fe41332b51465888033046b
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2018-02-06 10:55:07 +0100

    Complete package setup

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
         name='gym_gridworlds',
         version='0.0.0',
-        install_requires=['gym', 'numpy']
+        description='Gridworlds environments for OpenAI gym.',
+        author='Ondřej Podsztavek',
+        author_email='ondrej.podsztavek@gmail.com',
+        license='MIT License',
+        url='https://github.com/podondra/gym-gridworlds',
+        packages=find_packages(),
+        install_requires=['gym', 'numpy'],
+        setup_requires=['pytest-runner'],
+        tests_require=['pytest'],
         )
