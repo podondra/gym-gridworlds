4f9f3fe54b376c764bdc7b8010ba36de1522530c
squidszyd <squidszyd@outlook.com>
2024-02-15 10:48:09 +0800

    update

--- gym_gridworlds/envs/windy_gridworld_env.py
+++ gym_gridworlds/envs/windy_gridworld_env.py
@@ -3,20 +3,19 @@ from gym import spaces
 
 
 class WindyGridworldEnv(gym.Env):
+
     def __init__(self):
         self.height = 7
         self.width = 10
         self.action_space = spaces.Discrete(4)
-        self.observation_space = spaces.Tuple((
-                spaces.Discrete(self.height),
-                spaces.Discrete(self.width)
-                ))
+        self.observation_space = spaces.Tuple(
+            (spaces.Discrete(self.height), spaces.Discrete(self.width)))
         self.moves = {
-                0: (-1, 0),  # up
-                1: (0, 1),   # right
-                2: (1, 0),   # down
-                3: (0, -1),  # left
-                }
+            0: (-1, 0),  # up
+            1: (0, 1),  # right
+            2: (1, 0),  # down
+            3: (0, -1),  # left
+        }
 
         # begin in start state
         self.reset()
@@ -31,13 +30,15 @@ class WindyGridworldEnv(gym.Env):
         self.S = self.S[0] + x, self.S[1] + y
 
         self.S = max(0, self.S[0]), max(0, self.S[1])
-        self.S = (min(self.S[0], self.height - 1),
-                  min(self.S[1], self.width - 1))
+        self.S = (
+            min(self.S[0], self.height - 1),
+            min(self.S[1], self.width - 1),
+        )
 
         if self.S == (3, 7):
             return self.S, -1, True, {}
         return self.S, -1, False, {}
 
-    def reset(self):
+    def reset(self, seed=None, options=None):
         self.S = (3, 0)
-        return self.S
+        return self.S, {}
--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup, find_packages
 
-
 with open('README.rst') as f:
     long_description = ''.join(f.readlines())
 
 setup(
-        name='gym_gridworlds',
-        version='0.0.2',
-        description='Gridworlds environments for OpenAI gym.',
-        long_description=long_description,
-        author='Ondřej Podsztavek',
-        author_email='ondrej.podsztavek@gmail.com',
-        license='MIT License',
-        url='https://github.com/podondra/gym-gridworlds',
-        packages=find_packages(),
-        install_requires=['gym', 'numpy'],
-        setup_requires=['pytest-runner'],
-        tests_require=['pytest'],
-        )
+    name='gym_gridworlds',
+    version='0.0.3',
+    description='Gridworlds environments for OpenAI gym.',
+    long_description=long_description,
+    author='Ondřej Podsztavek',
+    author_email='ondrej.podsztavek@gmail.com',
+    license='MIT License',
+    url='https://github.com/podondra/gym-gridworlds',
+    packages=find_packages(),
+    install_requires=['gym', 'numpy'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
